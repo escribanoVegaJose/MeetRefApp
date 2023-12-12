73936a31a97bdfef4c7b2dd07d8db11bbe12cad7
Jose Escribano vega <jescribanov@indra.es>
2023-12-12 07:45:50 +0100

    [COMSEC-5144]: bottom_navigation_menu fixed load contacts issue

--- app/src/main/java/com/indra/iscs/meetrefapp/components/fragments/ContactsFragment.kt
+++ app/src/main/java/com/indra/iscs/meetrefapp/components/fragments/ContactsFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.indra.iscs.meetrefapp.R
 import com.indra.iscs.meetrefapp.XmppClientManager
 import com.indra.iscs.meetrefapp.components.adapters.RosterAdapter
+import org.jivesoftware.smack.roster.RosterEntry
 
 class ContactsFragment : Fragment() {
 
@@ -18,34 +19,44 @@ class ContactsFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var textViewNoContacts: TextView
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_contacts, container, false)
         recyclerView = view.findViewById(R.id.recyclerview_roster)
         textViewNoContacts = view.findViewById(R.id.textView_no_contacts)
         recyclerView.layoutManager = LinearLayoutManager(context)
         rosterAdapter = RosterAdapter(XmppClientManager.getInstance(), requireContext())
         recyclerView.adapter = rosterAdapter
-        XmppClientManager.getInstance().rosterUpdateListener = { updatedRoster ->
-                rosterAdapter.updateRoster(updatedRoster)
-                textViewNoContacts.visibility = if (updatedRoster.isEmpty()) View.VISIBLE else View.GONE
-        }
-        updateContactList()
         return view
     }
 
-    private fun updateContactList() {
-        val roster =XmppClientManager.getInstance().getRoster()
-        val entries = roster.entries?.toList()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRosterListener()
+    }
+
+    private fun setupRosterListener() {
+        XmppClientManager.getInstance().rosterUpdateListener = { updatedRoster ->
+            updateContactList(updatedRoster)  // Pasar directamente la lista de RosterEntry
+        }
+
+        // Cargar los datos actuales del roster
+        val currentRoster = XmppClientManager.getInstance().getRoster().entries?.toList()
+        updateContactList(currentRoster)
+    }
+
+    private fun updateContactList(entries: List<RosterEntry>?) {
         if (entries.isNullOrEmpty()) {
             textViewNoContacts.visibility = View.VISIBLE
         } else {
             textViewNoContacts.visibility = View.GONE
             rosterAdapter.updateRoster(entries)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 }
